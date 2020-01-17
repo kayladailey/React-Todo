037f3b807bf44ad9f4de0a2cbf7d5a4d60ea3517
Kayla Dailey <kayladailey000@gmail.com>
2020-01-17 00:50:47 -0600

    updated read me for completed components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import TodoList from './components/TodoComponents/TodoList';
 import TodoForm from './components/TodoComponents/TodoForm';
 
 class App extends React.Component {
@@ -21,9 +20,7 @@ class App extends React.Component {
       todo: ''
     };
   }
-  // you will need a place to store your state in this component.
-  // design `App` to be the parent component of your application.
-  // this component is going to take care of state, and any change handlers you need to work with your state
+  
   addTodo = e => {
     e.preventDefault();
     const newTodo = { task: this.state.todo,  };
